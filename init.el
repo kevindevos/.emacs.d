618860f86405c327772af1d8556c2c9862232f34
Kevin <kevindevos9542@gmail.com>
2018-09-22 20:42:34 +0100

    added configuration for company, neotree and linum-mode + keybinds

--- init.el
+++ init.el
@@ -8,7 +8,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (magit general helm evil use-package))))
+ '(package-selected-packages
+   (quote
+    (company neotree projectile magit general helm evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -18,10 +20,28 @@
 
 ;; CONSTANTS
 (defconst my-dotfile-path "~/.emacs.d/init.el")
+(defconst my-keybindings-file-path "~/.emacs.d/keybindings.el")
 
 ;; general configuration
 (tool-bar-mode -1) ;; disable gui tool bar
 (add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; default emacs to start maximized
+(set-scroll-bar-mode nil) ;; disable scrollbar in all buffers 
+(setq neo-show-hidden-files t)
+
+;; open linum-mode for all files
+(add-hook 'find-file-hook 'linum-mode)
+
+;; set default company backends ( does not include company-eclim company-xcode and some others that were included beforehand
+(use-package company
+  :config
+  (progn
+    (setq company-idle-delay 0.1)
+    (setq company-backends '(company-bbdb company-semantic company-clang company-capf company-keywords
+				      company-files company-dabbrev-code company-gtags company-etags 
+				      company-oddmuse company-dabbrev))
+    (global-company-mode)
+    )
+  )
 
 (require 'evil)
 (evil-mode 1)
@@ -37,9 +57,9 @@
 ;; general makes it easy to manage keybindings
 (use-package general
   :config
-  (load "~/.emacs.d/keybindings_general.el")
+  (load my-keybindings-file-path)
   )
 
-
-
-		
+;; TODO doom emacs speed?
+;; TODO yasnippet
+;; TODO flycheck
