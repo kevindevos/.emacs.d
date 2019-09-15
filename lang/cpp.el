fae6b15828fe028e8d72e94720e04fcf40c57e3f
Kevin <kevindevos9542@gmail.com>
2019-09-15 17:30:02 +0100

    Add external library to flycheck include for header files

--- lang/cpp.el
+++ lang/cpp.el
@@ -1,16 +1,17 @@
 (require 'flycheck)
-(flycheck-mode)				
+(use-package flycheck :ensure t
+  :config
+  (flymake-mode-off)
+  (flycheck-mode t)
+  (setq flycheck-clang-include-path (list "/Applications/JUCE/modules/"))
+  )
 
 (set-buffer-file-coding-system 'unix)
-
-(use-package lsp-mode :ensure t
-  :init
-  (lsp))
-
 
 (use-package cquery :ensure t
   :init
   (setq cquery-executable "/Users/kevindevos/cquery/build/cquery"))
 
-(flymake-mode-off)
-(flycheck-mode t)
+(use-package lsp-mode :ensure t
+  :config
+  (lsp))
